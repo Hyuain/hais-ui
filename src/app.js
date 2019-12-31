931e00822cadfdbd0cc811848609881c4b44076c
Hyuain <Hyuain97@163.com>
2019-12-31 16:16:36 +0800

    增加 row 和 col，可以进行简单分栏

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button.vue'
 import Icon from './icon.vue'
 import ButtonGroup from './button-group.vue'
 import Input from './input.vue'
+import Row from './row.vue'
+import Col from './col.vue'
 
 Vue.component('u-button', Button)
 Vue.component('u-icon', Icon)
 Vue.component('u-button-group', ButtonGroup)
 Vue.component('u-input', Input)
+Vue.component('u-row', Row)
+Vue.component('u-col', Col)
 
 
 new Vue({
---
+++ src/col.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HaiCol",
+    props: {
+      span: {
+        type: [Number, String]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .col {
+    height: 100px;
+    background: #b3cefb;
+    width: 50%;
+    border: 1px solid #666666;
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) *100%;
+      }
+    }
+  }
+</style>
