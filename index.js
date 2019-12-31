8407cf0c9390f01dbb56d0c324e1c9e68c1513a4
Hyuain <Hyuain97@163.com>
2019-12-31 12:43:13 +0800

    input 支持 v-model 双向绑定

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Button from './src/button.vue'
 import ButtonGroup from './src/button-group.vue'
 import Icon from './src/icon.vue'
+import Input from './src/input.vue'
 
-export {ButtonGroup, Button, Icon}
+export {ButtonGroup, Button, Icon, Input}
--- test/input.test.js
+++ test/input.test.js
@@ -78,9 +78,12 @@ describe('Input', () => {
           const callback = sinon.fake()
           vm.$on(eventName, callback)
           const event = new Event(eventName)
+          Object.defineProperty(event, 'target', {
+            value: {value: 'hi'}
+          })
           const inputElement = vm.$el.querySelector('input')
           inputElement.dispatchEvent(event)
-          expect(callback).to.have.been.calledWith(event)
+          expect(callback).to.have.been.calledWith(event.target.value)
         })
     })
   })
