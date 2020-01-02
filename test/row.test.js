256e9ab8238c140ba5dd07373cef4a511f318511
Hyuain <Hyuain97@163.com>
2020-01-02 10:27:57 +0800

    完成row col 测试用例

---
+++ test/row.test.js
@@ -0,0 +1,57 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Row from '../src/row.vue'
+import Col from '../src/col.vue'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Row', () => {
+
+  it('存在.', () => {
+    expect(Row).to.exist
+    expect(Col).to.exist
+  })
+  it('接收 gutter 属性.', (done) => {
+    // 不加 done 就认为是同步的
+    Vue.component('u-row', Row)
+    Vue.component('u-col', Col)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+      <u-row gutter="20">
+        <u-col span="12"></u-col>
+        <u-col span="12"></u-col>
+      </u-row>
+    `
+    const vm = new Vue({
+      el: div
+    })
+    setTimeout(() => {
+      const row = vm.$el.querySelector('.row')
+      expect(getComputedStyle(row).marginLeft).to.eq('-10px')
+      expect(getComputedStyle(row).marginRight).to.eq('-10px')
+      const cols = vm.$el.querySelectorAll('.col')
+      expect(getComputedStyle(cols[0]).paddingLeft).to.eq('10px')
+      expect(getComputedStyle(cols[1]).paddingRight).to.eq('10px')
+      vm.$el.remove()
+      vm.$destroy()
+      done()
+    })
+  })
+
+  it('接收 align 属性', () => {
+    const Constructor = Vue.extend(Row)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const vm = new Constructor({
+      propsData: {
+        align: 'center'
+      }
+    }).$mount(div)
+    const element = vm.$el
+    expect(getComputedStyle(element).justifyContent).to.equal('center')
+    vm.$destroy()
+    div.remove()
+  })
+})
