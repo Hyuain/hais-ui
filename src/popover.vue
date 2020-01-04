63223243bf4d0ed048ab3f58de15b56be3a23136
Hyuain <Hyuain97@163.com>
2020-01-04 10:44:58 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper" @click="onClickButton">
+    <span ref="triggerWrapper" class="trigger-wrapper" @click="onClickButton">
       <slot></slot>
     </span>
   </div>
@@ -25,7 +25,7 @@
           }
         }
       },
-      close(){
+      close() {
         this.visible = false
         document.removeEventListener('click', this.onClickDocument)
       },
@@ -42,7 +42,7 @@
         this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
         this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
       },
-      onClickDocument(event){
+      onClickDocument(event) {
         const needClose =
           (!this.$refs.contentWrapper || !this.$refs.contentWrapper.contains(event.target)) &&
           (!this.$refs.popover.contains(event.target)) &&
@@ -56,16 +56,43 @@
 </script>
 
 <style lang="scss" scoped>
+  $border-color: #333;
+  $border-radius: 4px;
   .popover {
     display: inline-block;
     vertical-align: top;
     position: relative;
+    .trigger-wrapper {
+      display: inline-block;
+    }
   }
   .content-wrapper {
     position: absolute;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, .5);
-    width: 100px;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
     transform: translateY(-100%);
+    margin-top: -10px;
+    max-width: 20em;
+    padding: .5em 1em;
+    word-break: break-all;
+    filter: drop-shadow(0 1px 1px rgba(0, 0, 0, .5));
+    background-color: white;
+    &::before,&::after{
+      content: '';
+      display: block;
+      border: 10px solid transparent;
+      width: 0;
+      height: 0;
+      position: absolute;
+      left: 10px;
+    }
+    &::before {
+      top: calc(100% + 1px);
+      border-top-color: $border-color;
+    }
+    &::after {
+      border-top-color: white;
+      top: 100%;
+    }
   }
 </style>
